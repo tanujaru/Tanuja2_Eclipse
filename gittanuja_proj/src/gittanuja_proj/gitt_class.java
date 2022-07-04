79d75ef0ae01889caff7553d20d59fecdfa49748
Ritu <tanujaru@gmail.com>
2022-07-04 13:13:06 -0400

    my second commit

--- gittanuja_proj/src/gittanuja_proj/gitt_class.java
+++ gittanuja_proj/src/gittanuja_proj/gitt_class.java
@@ -5,6 +5,7 @@ public class gitt_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my git projct");
+		System.out.println("my git projct2");
 
 	}
 
