d3bab81b5fb8ef010df4c7ac116605a7a2fb7625
Ritu <tanujaru@gmail.com>
2022-07-04 13:18:53 -0400

    mycommit 3

--- gittanuja_proj/src/gittanuja_proj/gitt_class.java
+++ gittanuja_proj/src/gittanuja_proj/gitt_class.java
@@ -4,8 +4,9 @@ public class gitt_class {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("my git projct");
+		
 		System.out.println("my git projct2");
+		System.out.println("my git projct3");
 
 	}
 
